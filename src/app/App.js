03532d2b82f6c9bf016304724bf04ce9748ed882
Abraham Echeverria <abrahamecheverria1999@gmail.com>
2023-02-16 21:42:11 -0600

    creando funcion para manejar el submit

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,25 @@
-import React from "react";  
+import React, { useState }from "react";  
 import Form from "./components/formulario"; 
-import ClimaInfo from "./components/climaInfo";
+import ClimaInfo from "./components/climaInfo"; 
+import useFetch from "./hooks/fetch";  
 
-export default function App () { 
+
+export default function App () {      
+    const {data, error, peticion,} = useFetch();  
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const { cityName, contryName } = e.target;
+      peticion(cityName.value, contryName.value);   
+      cityName.value = "" ; 
+      contryName.value = "" ;  
+    }; 
+
     return(<>  
     <h2>Clima XD</h2>    
     <div className="container-fluid p-0 bg-danger">
-        <Form />   
-        <ClimaInfo />
+        <Form handleSubmit={handleSubmit} />   
+        <ClimaInfo data={data} />
     </div> 
  
     </>)
