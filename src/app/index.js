bb1ef7ba535042d56aaf1200e564fb3bd512dd5a
Abraham Echeverria <abrahamecheverria1999@gmail.com>
2023-03-01 23:39:49 -0600

    añadiendo imagen al index.js

--- src/app/index.js
+++ src/app/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import bootstrap from "bootstrap"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"; 
+import "../assets/images/weatherApp.webp"
 
 const root = ReactDOM.createRoot(document.getElementById("App")); 
 root.render(
